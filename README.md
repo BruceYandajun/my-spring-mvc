68f2ca8d200782d2fb6a21444a581a6f04d55199
yandajun <yandajun@vipkid.com.cn>
2017-10-16 14:37:51 +0800

    commit simple files

---
+++ README.md
@@ -0,0 +1,2 @@
+# code_lib
+This is a lib of code
---
+++ src/test/java/com/github/bruce/base/StringSplitTest.java
@@ -0,0 +1,18 @@
+package com.github.bruce.base;
+
+/**
+ * Description
+ * <p>
+ * </p>
+ * DATE 10/15/17.
+ *
+ * @author yandajun.
+ */
+public class StringSplitTest {
+    public static void main(String[] args) {
+        String s = "";
+        for (String i : s.split(",")) {
+            System.out.println(i);
+        }
+    }
+}
