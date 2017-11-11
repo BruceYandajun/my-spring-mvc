78036f392eabeb2894cca98b9418d4068a3d4950
yandajun <yandajun@vipkid.com.cn>
2017-11-11 15:30:09 +0800

    delete target

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,12 @@
+package PACKAGE_NAME;
+
+/**
+ * Description
+ * <p>
+ * </p>
+ * DATE 11/11/17.
+ *
+ * @author yandajun.
+ */
+public class TestServlet {
+}
---
+++ src/test/java/com/github/bruce/base/LocalHostTest.java
@@ -0,0 +1,12 @@
+package com.github.bruce.base;
+
+/**
+ * Description
+ * <p>
+ * </p>
+ * DATE 10/19/17.
+ *
+ * @author yandajun.
+ */
+public class LocalHostTest {
+}
