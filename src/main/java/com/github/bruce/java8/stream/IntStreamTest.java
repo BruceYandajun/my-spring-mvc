3f0d91855573222461bc8727bf3a505e57945443
yandajun <yandajun@vipkid.com.cn>
2018-09-04 11:59:14 +0800

    IntStream.rangeClosed()

--- src/main/java/com/github/bruce/java8/stream/IntStreamTest.java
+++ src/main/java/com/github/bruce/java8/stream/IntStreamTest.java
@@ -5,11 +5,13 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static com.github.bruce.utils.BaseUtil.line;
+import static java.util.stream.Collectors.joining;
 
 public class IntStreamTest {
     public static void main(String[] args) {
         List<Integer> list = IntStream.range(0, 10).mapToObj(Integer::valueOf).collect(Collectors.toList());
         line(list);
-        IntStream.rangeClosed(0, 10).forEach(System.out::println);
+        String s = IntStream.rangeClosed(0, 10).boxed().map(i -> i.toString()).collect(joining(", "));
+        line(s);
     }
 }
