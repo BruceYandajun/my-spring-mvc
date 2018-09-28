ce9b616c643fc5f2c732fa42b5404a528d225bd4
yandajun <yandajun@vipkid.com.cn>
2018-09-28 14:32:36 +0800

    Predicate the person if being 20 years old

--- src/main/java/com/github/bruce/java8/optional/BooleanOptionalTest.java
+++ src/main/java/com/github/bruce/java8/optional/BooleanOptionalTest.java
@@ -7,11 +7,12 @@ import static com.github.bruce.utils.BaseUtil.line;
 public class BooleanOptionalTest {
 
     public static void main(String[] args) {
+        // 判断person是不是20岁
         Person person = new Person();
-        person.setAge(21);
-        person = null;
+        person.setAge(null);
+//        person = null;
         Optional<Person> optionalPerson = Optional.ofNullable(person);
-        boolean watched = optionalPerson.filter(p -> p.getAge() == 20).isPresent();
-        line(watched);
+        boolean result = optionalPerson.map(Person::getAge).filter(a -> a == 20).isPresent();
+        line(result);
     }
 }
