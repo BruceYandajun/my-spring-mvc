3110d5d891a3b02296055372f83cc776f1bf5c9d
yandajun <yandajun@vipkid.com.cn>
2020-08-26 21:15:11 +0800

    reduce

--- src/main/java/com/github/bruce/controller/ExcelController.java
+++ src/main/java/com/github/bruce/controller/ExcelController.java
@@ -1,15 +1,28 @@
 package com.github.bruce.controller;
 
+import lombok.SneakyThrows;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/excel")
 public class ExcelController {
 
+    @SneakyThrows
     @PostMapping("/upload")
-    public String upload() {
+    public String upload(@RequestParam("file") MultipartFile file) {
+        XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
+        XSSFSheet worksheet = workbook.getSheetAt(0);
+        for(int i = 1;i < worksheet.getPhysicalNumberOfRows() ;i++) {
+            XSSFRow row = worksheet.getRow(i);
+            System.out.println(row.getCell(0).getStringCellValue());
+        }
         return "upload";
     }
 }
--- src/main/java/com/github/bruce/java8/function/OperatorTest.java
+++ src/main/java/com/github/bruce/java8/function/OperatorTest.java
@@ -1,7 +1,11 @@
 package com.github.bruce.java8.function;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OperatorTest {
     public static void main(String[] args) {
@@ -12,5 +16,11 @@ public class OperatorTest {
         List<Integer> values = Arrays.asList(3, 5, 8, 9, 12);
         int sum = values.stream().reduce(0, (i1, i2) -> i1 + i2);
         System.out.println(sum);
+
+        List<List<Integer>> list = Arrays.asList(Arrays.asList(3, 5), Arrays.asList(4, 7));
+        List<Integer> integers = list.stream().reduce((a, b) -> Stream.of(a, b)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList())).get();
+        System.out.println(integers);
     }
 }
