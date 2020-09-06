8c295e1e4df7b91fdcfb8061bde69ee8bfddd2ed
yandajun <yandajun@vipkid.com.cn>
2020-09-06 16:19:49 +0800

    genericity

--- src/main/java/com/github/bruce/base/collections/ListGenericType.java
+++ src/main/java/com/github/bruce/base/collections/ListGenericType.java
@@ -2,18 +2,21 @@ package com.github.bruce.base.collections;
 
 import com.google.common.collect.Lists;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 public class ListGenericType {
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         // 一个带Integer泛型的list，如何add进一个String对象
         List<Integer> list = Lists.newArrayList();
         list.add(1);
-        Class<?> aClass = Class.forName("java.util.ArrayList");
-        List list1 = (List) aClass.newInstance();
-        list1.add("abc");
-        list.addAll(list1);
+        for (Method method : list.getClass().getMethods()) {
+            if ("add".equalsIgnoreCase(method.getName()) && method.getParameterCount() == 1) {
+                method.invoke(list, "abc");
+            }
+        }
         System.out.println(list);
     }
 }
