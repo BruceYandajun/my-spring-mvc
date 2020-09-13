b22d7468f8bbf372d61ea9f39ca01f06169ea75d
yandajun <yandajun@vipkid.com.cn>
2020-09-13 22:41:13 +0800

    lazy singleton

---
+++ src/main/java/com/github/bruce/designPattern/LazySingleton.java
@@ -0,0 +1,25 @@
+package com.github.bruce.designPattern;
+
+public class LazySingleton {
+
+    private volatile static LazySingleton instance;
+
+    private LazySingleton() {
+
+    }
+
+    public static LazySingleton getInstance() {
+        if (instance == null) {
+            synchronized (LazySingleton.class) {
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LazySingleton.getInstance());
+    }
+}
